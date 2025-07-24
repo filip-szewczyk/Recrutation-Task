fff88fd52540bc08f08751df0e87ca6e2b4728f0
filip-szewczyk <filip.szewczyk228@gmail.com>
2025-07-24 23:44:42 +0200

    Character Model with Animations
    Added model for player character with idle and movement animations.
    Also create two character variants.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,10 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody _rigidbody;
+    Animator _animator;
 
     public float MovementSpeed = 0.2f;
     Vector2 _movementValueVector;
@@ -12,10 +12,17 @@ public class PlayerController : MonoBehaviour
     public float RotationSpeed = 0.2f;
     Vector3 _lookVector;
 
+    int _diagonalMovementHash;
+    int _horizontalMovementHash;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+
+        _diagonalMovementHash = Animator.StringToHash("DiagonalMovement");
+        _horizontalMovementHash = Animator.StringToHash("HorizontalMovement");
     }
 
     // Update is called once per frame
@@ -26,8 +33,8 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 forwardMovementVector = -transform.forward * _movementValueVector.x;
-        Vector3 sideMovementVector = transform.right * _movementValueVector.y;
+        Vector3 forwardMovementVector = transform.forward * _movementValueVector.y * MovementSpeed;
+        Vector3 sideMovementVector = transform.right * _movementValueVector.x * MovementSpeed;
 
         _rigidbody.MovePosition(_rigidbody.position + forwardMovementVector + sideMovementVector);
         transform.Rotate(_lookVector);
@@ -35,12 +42,15 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue movementValue)
     {
-        _movementValueVector = movementValue.Get<Vector2>() * MovementSpeed;
+        _movementValueVector = movementValue.Get<Vector2>();
+
+        _animator.SetFloat(_diagonalMovementHash, -_movementValueVector.x);
+        _animator.SetFloat(_horizontalMovementHash, _movementValueVector.y);
     }
 
     void OnLook(InputValue lookValue)
     {
-        Vector2 lookValueVector = lookValue.Get<Vector2>() * RotationSpeed;
-        _lookVector = new Vector3(0, lookValueVector.x, 0);
+        Vector2 lookValueVector = lookValue.Get<Vector2>();
+        _lookVector = new Vector3(0, lookValueVector.x, 0) * RotationSpeed;
     }
 }
