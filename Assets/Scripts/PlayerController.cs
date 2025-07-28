88ab2a3d4bd6565b070763936d2661307e560dc3
filip-szewczyk <filip.szewczyk228@gmail.com>
2025-07-28 23:45:06 +0200

    Improved Controls
    Refactored Move and Add Second Axis in Camera Roation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,39 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody _rigidbody;
     Animator _animator;
 
     public float MovementSpeed = 0.2f;
-    Vector2 _movementValueVector;
+    Vector3 _movement;
 
+    public GameObject CameraTarget;
     public float RotationSpeed = 0.2f;
-    Vector3 _lookVector;
+    public float MinCameraPitch = 30f;
+    public float MaxCameraPitch = 70f;
+    float _targetYaw;
+    float _targetPitch;
+    Vector3 _rotation;
 
     int _diagonalMovementHash;
     int _horizontalMovementHash;
 
+    float _threshold = 0.1f;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _animator = GetComponent<Animator>();
-
         _diagonalMovementHash = Animator.StringToHash("DiagonalMovement");
         _horizontalMovementHash = Animator.StringToHash("HorizontalMovement");
+
+        _targetYaw = CameraTarget.transform.rotation.eulerAngles.y;
     }
 
     // Update is called once per frame
@@ -33,24 +44,54 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 forwardMovementVector = transform.forward * _movementValueVector.y * MovementSpeed;
-        Vector3 sideMovementVector = transform.right * _movementValueVector.x * MovementSpeed;
-
-        _rigidbody.MovePosition(_rigidbody.position + forwardMovementVector + sideMovementVector);
-        transform.Rotate(_lookVector);
+        Move();
+        Rotate();
     }
 
-    void OnMove(InputValue movementValue)
+    void Move()
     {
-        _movementValueVector = movementValue.Get<Vector2>();
+        if (_movement.magnitude > _threshold)
+        {
+            float angle = (float)Math.Atan2(_movement.x, _movement.z) * Mathf.Rad2Deg + CameraTarget.transform.eulerAngles.y;
+            Vector3 direction = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
 
-        _animator.SetFloat(_diagonalMovementHash, -_movementValueVector.x);
-        _animator.SetFloat(_horizontalMovementHash, _movementValueVector.y);
+            transform.rotation = Quaternion.Euler(0f, _targetYaw, 0f);
+            transform.position += direction.normalized * MovementSpeed;
+        }
     }
 
-    void OnLook(InputValue lookValue)
+    void Rotate()
     {
-        Vector2 lookValueVector = lookValue.Get<Vector2>();
-        _lookVector = new Vector3(0, lookValueVector.x, 0) * RotationSpeed;
+        _targetYaw += _rotation.y * RotationSpeed;
+        _targetPitch += _rotation.z * RotationSpeed;
+
+        _targetYaw = ClampAngle(_targetYaw, float.MinValue, float.MaxValue);
+        _targetPitch = ClampAngle(_targetPitch, MinCameraPitch, MaxCameraPitch);
+
+        CameraTarget.transform.rotation = Quaternion.Euler(_targetPitch, _targetYaw, 0f);
+    }
+
+    float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360f)
+            angle += 360f;
+        if (angle > 360f)
+            angle -= 360f;
+
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    void OnMove(InputValue input)
+    {
+        Vector2 movementValue = input.Get<Vector2>();
+        _movement = new Vector3(movementValue.x, 0f, movementValue.y);
+
+        _animator.SetFloat(_diagonalMovementHash, -movementValue.x);
+        _animator.SetFloat(_horizontalMovementHash, movementValue.y);
+    }
+    void OnLook(InputValue input)
+    {
+        Vector2 lookValue = input.Get<Vector2>();
+        _rotation = new Vector3(0, lookValue.x, lookValue.y);
     }
 }
